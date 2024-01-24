ef2fddafd61922a5c0280c5d178bd24550aa61c1
homeIg <dlsrb991118249@gmail.com>
2024-01-24 22:08:22 +0900

    Initial commit

---
+++ src/main/java/com/research_group/bootjpasettings/BootJpaSettingsApplication.java
@@ -0,0 +1,13 @@
+package com.research_group.bootjpasettings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaSettingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJpaSettingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/research_group/bootjpasettings/BootJpaSettingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research_group.bootjpasettings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJpaSettingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
